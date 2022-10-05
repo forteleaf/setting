6c43dc728045918495da3322b3030ac52319b2eb
hwang jung hwan <forteleaf@gmail.com>
2022-10-05 18:54:11 +0900

    Create config.fish
    fish shell 설정

---
+++ config.fish
@@ -0,0 +1,17 @@
+kubectl completion fish | source
+alias g=git
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+set --export PATH $HOME/.cargo/bin $PATH
+
+# prompt
+#starship init fish | source
+
+# exa ( ls changer )
+alias l="exa --icons -h"
+alias ls="exa --icons --long -h"
+alias la="exa --all --long --icons -h"
+
+export FZF_DEFAULT_COMMAND='fd --type f'
